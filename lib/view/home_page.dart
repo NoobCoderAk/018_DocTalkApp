da39ec8c1d03cad4f4e5b7177944db1ef2a7de60
NoobCoderAk <abidakbar0001@gmail.com>
2023-06-24 23:56:17 +0700

    create UI on home page

---
+++ lib/view/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:chatapp/view/login_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to the Home Page',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(builder: (context) => const LoginPage()),
+                  (Route<dynamic> route) => false,
+                );
+              },
+              child: const Text('Sign Out'),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
