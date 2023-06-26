ebb59ccb394ccf5d937e17f191322e54040eb96d
NoobCoderAk <abidakbar0001@gmail.com>
2023-06-26 23:39:11 +0700

    add feature search user

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,30 +1,41 @@
 import 'package:chatapp/view/login_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../controller/auth_google.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController _search = TextEditingController();
+  QuerySnapshot? _searchResults;
+
+  void onSearch() async {
+    FirebaseFirestore useraccount = FirebaseFirestore.instance;
+
+    QuerySnapshot searchSnapshot = await useraccount
+        .collection('users')
+        .where("email", isEqualTo: _search.text)
+        .get();
+    setState(() {
+      _searchResults = searchSnapshot;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Welcome to the Home Page',
-              style: TextStyle(fontSize: 24),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('DocTalk App'),
+          backgroundColor: Colors.red,
+          actions: [
+            IconButton(
               onPressed: () {
                 AuthGoogle().signOut();
                 Navigator.of(context).pushAndRemoveUntil(
@@ -32,11 +43,64 @@ class _HomePageState extends State<HomePage> {
                   (Route<dynamic> route) => false,
                 );
               },
-              child: const Text('Sign Out'),
+              icon: const Icon(Icons.logout),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(right: 10),
+                  height: 50,
+                  alignment: Alignment.topCenter,
+                  child: Container(
+                    height: 50,
+                    width: 200,
+                    alignment: Alignment.center,
+                    child: TextField(
+                      controller: _search,
+                      textAlign: TextAlign.start,
+                      decoration: InputDecoration(
+                        hintText: "Cari",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: onSearch,
+                  child: const Text('Cari'),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _searchResults?.docs.length ?? 0,
+                itemBuilder: (context, index) {
+                  final userData = _searchResults?.docs[index].data()
+                      as Map<String, dynamic>;
+
+                  return ListTile(
+                    title: Text(userData['displayName']),
+                    subtitle: Text(userData['email']),
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
-    ));
+    );
   }
 }
