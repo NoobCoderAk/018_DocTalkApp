21d6c17fc54ee0e7cf7112aa19437c40b3127e93
NoobCoderAk <abidakbar0001@gmail.com>
2023-06-30 19:04:38 +0700

    fix data bugs on update profile page

--- lib/view/update_profile.dart
+++ lib/view/update_profile.dart
@@ -1,53 +1,63 @@
-// ignore_for_file: use_build_context_synchronously, library_private_types_in_public_api
+// ignore_for_file: use_build_context_synchronously
 
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class UpdateProfile extends StatefulWidget {
-  const UpdateProfile(
-      {super.key,
-      required displayName,
-      required name,
-      required email,
-      required address,
-      required spesialisasi,
-      required license,
-      required bio});
+  final String? displayName;
+  final String? name;
+  final String? email;
+  final String? address;
+  final String? spesialisasi;
+  final String? license;
+  final String? bio;
+
+  const UpdateProfile({
+    Key? key,
+    required this.displayName,
+    required this.name,
+    required this.email,
+    required this.address,
+    required this.spesialisasi,
+    required this.license,
+    required this.bio,
+  }) : super(key: key);
 
   @override
   _UpdateProfileState createState() => _UpdateProfileState();
 }
 
 class _UpdateProfileState extends State<UpdateProfile> {
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _addressController = TextEditingController();
-
-  @override
-  void dispose() {
-    _nameController.dispose();
-    _addressController.dispose();
-    super.dispose();
-  }
-
+  late TextEditingController _displayNameController;
+  late TextEditingController _nameController;
+  late TextEditingController _emailController;
+  late TextEditingController _addressController;
+  late TextEditingController _spesialisasiController;
+  late TextEditingController _lisensiController;
+  late TextEditingController _bioController;
   @override
   void initState() {
     super.initState();
-    // Load current user data
-    final currentUser = FirebaseAuth.instance.currentUser;
-    final currentUserId = currentUser!.uid;
+    _displayNameController = TextEditingController(text: widget.displayName);
+    _nameController = TextEditingController(text: widget.name);
+    _emailController = TextEditingController(text: widget.email);
+    _addressController = TextEditingController(text: widget.address);
+    _spesialisasiController = TextEditingController(text: widget.spesialisasi);
+    _lisensiController = TextEditingController(text: widget.license);
+    _bioController = TextEditingController(text: widget.bio);
+  }
 
-    FirebaseFirestore.instance
-        .collection('users')
-        .doc(currentUserId)
-        .get()
-        .then((DocumentSnapshot documentSnapshot) {
-      if (documentSnapshot.exists) {
-        final userData = documentSnapshot.data() as Map<String, dynamic>;
-        _nameController.text = userData['name'];
-        _addressController.text = userData['address'];
-      }
-    });
+  @override
+  void dispose() {
+    _displayNameController.dispose();
+    _nameController.dispose();
+    _emailController.dispose();
+    _addressController.dispose();
+    _spesialisasiController.dispose();
+    _lisensiController.dispose();
+    _bioController.dispose();
+    super.dispose();
   }
 
   void _updateUserData() async {
@@ -56,11 +66,16 @@ class _UpdateProfileState extends State<UpdateProfile> {
 
     try {
       await FirebaseFirestore.instance
-          .collection('users')
+          .collection('Registration Files')
           .doc(currentUserId)
           .update({
+        'displayName': _displayNameController.text,
         'name': _nameController.text,
+        'email': _emailController.text,
         'address': _addressController.text,
+        'spesialisasi': _spesialisasiController.text,
+        'license': _lisensiController.text,
+        'bio': _bioController.text,
       });
       showDialog(
         context: context,
@@ -111,9 +126,25 @@ class _UpdateProfileState extends State<UpdateProfile> {
         child: Column(
           children: [
             TextFormField(
+              enabled: true,
+              controller: _displayNameController,
+              decoration: const InputDecoration(
+                labelText: 'Nama Panggilan',
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
               controller: _nameController,
               decoration: const InputDecoration(
-                labelText: 'Name',
+                labelText: 'Nama Lengkap',
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              enabled: false,
+              controller: _emailController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
               ),
             ),
             const SizedBox(height: 16.0),
@@ -121,6 +152,27 @@ class _UpdateProfileState extends State<UpdateProfile> {
               controller: _addressController,
               decoration: const InputDecoration(
                 labelText: 'Address',
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              controller: _spesialisasiController,
+              decoration: const InputDecoration(
+                labelText: 'Spesialisasi',
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              controller: _lisensiController,
+              decoration: const InputDecoration(
+                labelText: 'Surat Izin Praktek',
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              controller: _bioController,
+              decoration: const InputDecoration(
+                labelText: 'Bio',
               ),
             ),
             const SizedBox(height: 16.0),
